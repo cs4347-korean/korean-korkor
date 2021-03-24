f45f4a7ef88a9b5b89d277aa3fed90af0ab9c0a9
wfxronald <ronald@comp.nus.edu.sg>
2021-03-24 19:10:40 +0800

    Implement evaluation using Google STT

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, make_response
+from base64 import b64encode
+import json, requests, re
+from kaldialign import align
+from datetime import datetime
 
 app = Flask(__name__)
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+google_api_key = os.environ['GOOGLE_API_KEY']
 
 @app.route("/")
 def home():
@@ -21,10 +26,56 @@ def record():
 @app.route("/upload", methods=['POST'])
 def upload():
     f = request.files['file']
-    with open('audio/recording.wav', 'wb') as audio:
+    base64_str = b64encode(f.read()).decode('utf-8')
+
+    canonical = request.form['canonical'].strip()
+
+    # Get pure hangeul
+    cleaned_canonical = ""
+    for char in canonical:
+        if re.match("[\uac00-\ud7a3]", char):
+            cleaned_canonical += char
+
+    url = 'https://speech.googleapis.com/v1p1beta1/speech:recognize?key=' + google_api_key
+    json_body = {
+        "audio": {
+            "content": base64_str
+        },
+        "config": {
+            "enableAutomaticPunctuation": False,
+            "encoding": "LINEAR16",
+            "languageCode": "ko-KR",
+            "model": "default"
+        }
+    }
+
+    res = requests.post(url, data=json.dumps(json_body))
+
+    # Get the transcription with the highest confidence
+    trans = res.json()['results'][0]['alternatives'][0]['transcript']
+    conf = res.json()['results'][0]['alternatives'][0]['confidence']
+
+    # Clean transcript
+    cleaned_trans = ""
+    for char in trans:
+        if re.match("[\uac00-\ud7a3]", char):
+            cleaned_trans += char
+
+    matched_text = align(cleaned_canonical, cleaned_trans, "*")
+
+    to_return = {
+        "transcription": trans,
+        "confidence": conf,
+        "matched_text": matched_text
+    }
+
+    # Save file in database for analysis purpose
+    filename = datetime.today().strftime('%Y-%m-%d_%H:%M:%S')
+    with open('audio/' + filename + '.wav', 'wb') as audio:
         f.save(audio)
     print('Saved audio file')
-    return 'Success', 200
+
+    return make_response(jsonify(to_return), 200)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
--- static/record.js
+++ static/record.js
@@ -65,7 +65,7 @@ function startRecording() {
 			Create the Recorder object and configure to record mono sound (1 channel)
 			Recording 2 channels  will double the file size
 		*/
-		rec = new Recorder(input,{numChannels: 2})
+		rec = new Recorder(input,{numChannels: 1})
 
 		//start the recording process
 		rec.record()
@@ -108,12 +108,16 @@ async function uploadRecording() {
         if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
             if (xmlhttp.status == 200) {
                 console.log('200 success returned')
+                var json = JSON.parse(xmlhttp.responseText);
+                showResults(json);
             }
             else if (xmlhttp.status == 400) {
                 console.log('There was an error 400');
+                showError();
             }
             else {
                 console.log('something else other than 200 was returned');
+                showError();
             }
         }
     };
@@ -122,11 +126,13 @@ async function uploadRecording() {
 	var url = document.getElementsByTagName("audio")[0].getAttribute("src");
 	let blob = await fetch(url).then(r => r.blob());
 	fd.append('file', blob);
+	var canonical = document.getElementById("canonical").innerHTML
+	fd.append('canonical', canonical)
 	
 	xmlhttp.open("POST", "/upload", true);
 	xmlhttp.send(fd);
 	
-	document.getElementById("recordingContainer").innerHTML = 'Uploading succeeded!';
+	document.getElementById("recordingContainer").innerHTML = 'Uploading succeeded! Loading for results below...';
 	recordButton.disabled = false;
 	stopButton.disabled = true;
 	uploadButton.disabled = true;
@@ -147,4 +153,39 @@ function createDownloadLink(blob) {
 	//add the li element to the ol
 	document.getElementById("recordingContainer").innerHTML = '';
 	document.getElementById("recordingContainer").append(div);
+}
+
+function showResults(json) {
+	var list = json.matched_text;
+	var canonical = document.getElementById('canonical').innerHTML;
+	var korRegex = "[\uac00-\ud7a3]";
+	var finalText = '<strong>Transcription: </strong>';
+	var listCount = 0;
+
+	// Process the text
+	for (let i = 0; i < canonical.length; i++) {
+		var currChar = canonical[i];
+		if (currChar.match(korRegex)) {  // Only match the Korean characters
+			var pair = list[listCount];
+			if (pair[0] == pair[1]) {
+				finalText += pair[0]
+			} else {
+				finalText += '<strong class="text-danger">' + pair[0] + '</strong>'
+			}
+
+			listCount++;
+		} else {  // It is a punctuation, space, etc
+			finalText += currChar
+		}
+	}
+	
+	document.getElementById("transcription").innerHTML = finalText;
+	document.getElementById("confidence").innerHTML = '<strong>Confidence: </strong>' + json.confidence;
+	document.getElementById("recordingContainer").innerHTML = '';
+}
+
+function showError() {
+	document.getElementById("transcription").innerHTML = 'An error has occurred. Please try recording again!';
+	document.getElementById("confidence").innerHTML = '';
+	document.getElementById("recordingContainer").innerHTML = '';
 }
